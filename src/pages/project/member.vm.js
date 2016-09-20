c0ab252525e9184628875e3a543c3d246e3dfee7
songkaiyu <songkaiyu@36kr.com>
2016-09-20 10:20:37 +0800

    对接口

--- src/pages/project/member.vm.js
+++ src/pages/project/member.vm.js
@@ -1,5 +1,18 @@
 export default class MemberVM {
   constructor(data) {
-    angular.extend(this, data);
+    this.data = data;
+    this.init();
+  }
+  init() {
+    if (this.data.members.length > 1) {
+      this.data.membersList = this.data.members.slice(0, 1);
+      this.moreShow = true;
+    }
+    function more() {
+      this.data.membersList = this.data.members;
+      this.moreShow = false;
+      console.log('list', this.data.membersList, this.moreShow);
+    }
+    this.more = more;
   }
 }
