8d876e20a071bdfd731f21cef49a93c168d0de24
songkaiyu <songkaiyu@36kr.com>
2016-09-20 10:20:37 +0800

    revert projectData

--- src/pages/project/index.controller.js
+++ src/pages/project/index.controller.js
@@ -4,21 +4,18 @@ import FinanceVM from './finance.vm';
 import MemberVM from './member.vm';
 import SimilarVM from './similar.vm';
 import NewsVM from './news.vm';
-@Inject('$stateParams', 'projectService', '$injector')
+@Inject('$stateParams', 'projectService', 'projectData')
 export default class ProjectIndexController {
   constructor() {
     this.init();
   }
 
   init() {
-    if (this.$injector.has('projectData')) {
-      this.projectData = this.$injector.get('projectData');
-      this.baseInfoVM = new BaseInfoVM(this.projectData.baseInfo);
-      this.fundsVM = new FundsVM(this.projectData.funds);
-      this.financeVM = new FinanceVM(this.projectData.finance);
-      this.memberVM = new MemberVM(this.projectData.member);
-      this.similarVM = new SimilarVM(this.projectData.similar);
-      this.newsVM = new NewsVM(this.projectData.news);
-    }
+    this.baseInfoVM = new BaseInfoVM(this.projectData.baseInfo);
+    this.fundsVM = new FundsVM(this.projectData.funds);
+    this.financeVM = new FinanceVM(this.projectData.finance);
+    this.memberVM = new MemberVM(this.projectData.member);
+    this.similarVM = new SimilarVM(this.projectData.similar);
+    this.newsVM = new NewsVM(this.projectData.news);
   }
 }
