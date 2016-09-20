2b444680d2629a0287a498bf06c3174481ede06a
songkaiyu <songkaiyu@36kr.com>
2016-09-20 09:57:38 +0800

    dynamic get projectData

--- src/pages/project/index.controller.js
+++ src/pages/project/index.controller.js
@@ -4,18 +4,21 @@ import FinanceVM from './finance.vm';
 import MemberVM from './member.vm';
 import SimilarVM from './similar.vm';
 import NewsVM from './news.vm';
-@Inject('$stateParams', 'projectService', 'projectData')
+@Inject('$stateParams', 'projectService', '$injector')
 export default class ProjectIndexController {
   constructor() {
     this.init();
   }
 
   init() {
-    this.baseInfoVM = new BaseInfoVM(this.projectData.baseInfo);
-    this.fundsVM = new FundsVM(this.projectData.funds);
-    this.financeVM = new FinanceVM(this.projectData.finance);
-    this.memberVM = new MemberVM(this.projectData.member);
-    this.similarVM = new SimilarVM(this.projectData.similar);
-    this.newsVM = new NewsVM(this.projectData.news);
+    if (this.$injector.has('projectData')) {
+      this.projectData = this.$injector.get('projectData');
+      this.baseInfoVM = new BaseInfoVM(this.projectData.baseInfo);
+      this.fundsVM = new FundsVM(this.projectData.funds);
+      this.financeVM = new FinanceVM(this.projectData.finance);
+      this.memberVM = new MemberVM(this.projectData.member);
+      this.similarVM = new SimilarVM(this.projectData.similar);
+      this.newsVM = new NewsVM(this.projectData.news);
+    }
   }
 }
--- src/pages/search/index.controller.js
+++ src/pages/search/index.controller.js
@@ -1,6 +1,4 @@
 @Inject('$log')
 export default class SearchIndexController {
-  constructor() {
-    this.$log.info(1);
-  }
+
 }
