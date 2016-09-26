ce3a542eff40eab714835e9d56bb10e0ac7b9a34
zhangliansheng <zls0926@gmail.com>
2016-09-26 19:11:34 +0800

    融资经历添加表单验证

--- src/pages/project/finance.vm.js
+++ src/pages/project/finance.vm.js
@@ -6,10 +6,34 @@ export default class FinanceVM extends krData.FormVM {
     this.initData(data);
     this.projectService = krData.utls.getService('projectService');
   }
+
   initData(data) {
     angular.extend(this, data);
     // this.watch();
   }
+
+  setData(data) {
+    this.originalData = {};
+    angular.copy(data, this.originalData);
+  }
+
+  recovery() {
+    angular.extend(this, this.originalData);
+  }
+
+  // watchName() {
+  //   const that = this;
+  //   this.$scope.$watch('vm.baseInfoVM.name', function watchName(nv, ov) {
+  //     if (nv !== ov) {
+  //       that.$validation.validate(that.form.fullName);
+  //     }
+  //   });
+  // }
+
+  // watch() {
+  //   this.watchName();
+  // }
+
   init() {
     let num = 1;
     function getlist(limitlist, list, n) {
@@ -31,5 +55,20 @@ export default class FinanceVM extends krData.FormVM {
 
     this.more = more;
     this.showMore = showMore;
+  }
+
+  update($event) {
+    if (!this.validate($event)) return;
+    this.projectService.editHeader({
+      id: this.id,
+    }, this.getCopy(['phase',
+      'financeAmount',
+      'financeAmountUnit',
+      'entityName',
+    ]))
+      .then(() => {
+        this.recovery();
+        this.ok();
+      });
   }
 }
