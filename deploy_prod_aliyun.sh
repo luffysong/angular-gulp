f52119336d60351115fd3facaa3bcf059a7af27c
xiaobao <luxinyue@36kr.com>
2016-10-11 09:40:27 +0800

    chore: 添加生产环境脚本

---
+++ deploy_prod_aliyun.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+npm run prod
+
+cdnstr="/data/work/asset/nrong"
+####### CDN
+
+#site1
+rsync -rvltOD ./dist/* ali-rong-proxy-01:$cdnstr
+rsync -rvltOD ./dist/* ali-rong-api-01:$cdnstr
+
+#site2
+rsync -rvltOD ./dist/* ali-rong-proxy-02:$cdnstr
+rsync -rvltOD ./dist/* ali-rong-api-02:$cdnstr
+
+
+###### 业务逻辑
+pathstr="/data/work/frontend/prod/36kr/nrong/dist"
+if [ $1 = 'site1' ]; then
+    rsync -rvltOD ./dist/* ali-rong-proxy-01:$pathstr
+elif [ $1 = 'site2' ]; then
+    rsync -rvltOD ./dist/* ali-rong-proxy-02:$pathstr
+elif [ $1 = 'sim' ]; then
+    rsync -rvltOD ./dist/* ali-rong-sim-01:$pathstr
+elif [ $1 = 'all' ]; then
+    rsync -rvltOD ./dist/* ali-rong-proxy-01:$pathstr
+    rsync -rvltOD ./dist/* ali-rong-proxy-02:$pathstr
+    rsync -rvltOD ./dist/* ali-rong-sim-01:$pathstr
+fi
+
