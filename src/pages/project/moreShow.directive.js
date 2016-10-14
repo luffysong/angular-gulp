0d19d3ca957b80efcef7ec87e1cbca20126fc8e9
马超越 <machaoyue@36kr.com>
2016-10-14 17:23:51 +0800

    moreshow

--- src/pages/project/moreShow.directive.js
+++ src/pages/project/moreShow.directive.js
@@ -9,48 +9,19 @@ function moreShowDirective($timeout, $rootScope) {
     link(scope, element) {
       const more = $('<div class="more" >查看更多</div>');
       scope.funds = scope.text;
-      // element.text(template);
-      // console.log(height);
-      // if (height > 150) {
-        // element.append(more);
-      // } else {
-      //   return;
-      // }
-      // element.text(scope.text);
-      // element.append(more);
       $timeout(() => {
-        console.log(element.height());
+        const height = element.height();
+        if (height > 150) {
+          element.parent().parent().append(more);
+        } else {
+          return;
+        }
       }, 100);
-      // if(scope.text){
-      //   console.log(scope.text);
-      //   element.append(more);
-      // }
-      function createDots() {
-        element.dotdotdot({
-          after: 'a.toggle',
-        });
-      }
-      createDots();
-      function destroyDots() {
-        element.trigger('destroy');
-      }
-
-      element.on('click',
-        'a.toggle',
-        function moreClick() {
-          element.toggleClass('opened');
-
-          if (element.hasClass('opened')) {
-            destroyDots();
-          } else {
-            createDots();
-          }
-          return false;
-        });
+      more.click(function moreClick() {
+        more.css('display', 'none');
+        element.parent().css('height', 'auto');
+      });
     },
-    // controller($scope) {
-    //   $scope.funds = $scope.text;
-    // },
   };
 }
 angular
