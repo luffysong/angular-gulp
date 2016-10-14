4ea964446154fd69b8ff4b9c0621ed0b316b62ee
马超越 <machaoyue@36kr.com>
2016-10-14 14:22:26 +0800

    directive

--- src/pages/project/moreShow.directive.js
+++ src/pages/project/moreShow.directive.js
@@ -1,24 +1,59 @@
 
-function moreShowDirective() {
+function moreShowDirective($timeout, $rootScope) {
   return {
     restrict: 'AE',
+    scope: {
+      text: '=',
+    },
+    templateUrl: '/pages/project/templates/financeInfo.html',
     link(scope, element) {
       const more = $('<div class="more" >查看更多</div>');
-      const height = element.height();
-      if (height > 150) {
-        element.parent().parent().append(more);
-      } else {
-        return;
+      scope.funds = scope.text;
+      // element.text(template);
+      // console.log(height);
+      // if (height > 150) {
+        // element.append(more);
+      // } else {
+      //   return;
+      // }
+      // element.text(scope.text);
+      // element.append(more);
+      $timeout(() => {
+        console.log(element.height());
+      }, 100);
+      // if(scope.text){
+      //   console.log(scope.text);
+      //   element.append(more);
+      // }
+      function createDots() {
+        element.dotdotdot({
+          after: 'a.toggle',
+        });
       }
-      more.click(function showAll() {
-        element.parent().css('height', 'auto');
-        more.css('display', 'none');
-      });
+      createDots();
+      function destroyDots() {
+        element.trigger('destroy');
+      }
+
+      element.on('click',
+        'a.toggle',
+        function moreClick() {
+          element.toggleClass('opened');
+
+          if (element.hasClass('opened')) {
+            destroyDots();
+          } else {
+            createDots();
+          }
+          return false;
+        });
     },
+    // controller($scope) {
+    //   $scope.funds = $scope.text;
+    // },
   };
 }
 angular
   .module('@@pages.project', [])
   .directive('moreShow', moreShowDirective);
-
 export default moreShowDirective;
