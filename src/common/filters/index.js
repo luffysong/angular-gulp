d434fa070f5df0f1f236bbc1d303174891e45a9b
马超越 <machaoyue@36kr.com>
2016-10-17 14:57:17 +0800

    bugs

--- src/common/filters/index.js
+++ src/common/filters/index.js
@@ -4,6 +4,7 @@ import { getConstantFilterFactory } from '../base/utls';
 
 angular.module('@@app.filters', [])
   .filter('financePahse', getConstantFilterFactory(META.FINANCE_PHASE_META))
+  .filter('fundsPhaseEnum', getConstantFilterFactory(META.FUNDS_PHASE_ENUM_META))
   .filter('operationStatus', getConstantFilterFactory(META.OPERATION_STATUS_META))
   .filter('currencyUnit', getConstantFilterFactory(META.CURRENCY_UNIT_META))
   .filter('companyFinancePhase', getConstantFilterFactory(META.COMPANY_FINANCE_PHASE_META))
