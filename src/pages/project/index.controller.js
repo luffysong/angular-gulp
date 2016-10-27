104c3df1347dee3e108a148396bba944e541652d
zhangliansheng <zls0926@gmail.com>
2016-10-27 20:47:36 +0800

    fix: 融资设置入口添加状态验证

--- src/pages/project/index.controller.js
+++ src/pages/project/index.controller.js
@@ -9,6 +9,7 @@ import ProductVM from './product.vm';
 import ClaimVM from './claim.vm';
 import CollectionVM from './collection.vm';
 const FINANCE_ALLOW = '1';
+const FINANCE_AUDITING = '2';
 const BP_PERMISSION = {
   APPLY: 'APPLY',
   AGREE: 'AGREE',
@@ -50,7 +51,7 @@ export default class ProjectIndexController {
     };
     this.projectService.fundState(id)
     .then(data => {
-      if (data.state === FINANCE_ALLOW) {
+      if (data.state === FINANCE_ALLOW || data.state === FINANCE_AUDITING) {
         this.fundsState = true;
       }
     });
