bee3761165e1cf3a2890839a295b2406185caa6e
songkaiyu <songkaiyu@36kr.com>
2016-10-28 16:16:07 +0800

    add column directive

--- src/pages/home/index.controller.js
+++ src/pages/home/index.controller.js
@@ -1,32 +1,14 @@
-import krData from 'krData';
-
-class TestAPI extends krData.API {
-
-}
-
 @Inject('indexService', '$timeout', '$window')
 export default class IndexController {
 
   constructor() {
-    this.api = new TestAPI();
     this.init();
     this.columnOptions = {};
   }
-
   init() {
-    this.tags = ['旅游', 'isFunding',
-      '电子商务', '的三大大',
-      'savegroup', 'testst',
-    ];
-    this.loadDataBind = this.loadData.bind(this);
-    this.isFunding = true;
-    this.params = {
-      phase: 'A',
-    };
+
   }
-  loadData() {
-    krData.utls.getService('$log').info('text:', this.params);
-  }
+
 
 }
 
