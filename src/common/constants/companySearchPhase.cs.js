b763babe7bdbe968513ad756063beecc2c2b9ec8
zhouxiao <zhouxiao@36kr.com>
2016-10-31 14:44:16 +0800

    fix:字典更新

--- src/common/constants/companySearchPhase.cs.js
+++ src/common/constants/companySearchPhase.cs.js
@@ -1,75 +1,50 @@
 import { extractMeta } from '../base/utls';
 const COMPANY_SEARCH_PHASE_META = [
   {
-    desc: '未知轮次',
-    id: -100,
-    value: 'UNKNOWN',
+    "desc": "天使轮",
+    "id": 10,
+    "value": "ANGEL"
   },
   {
-    desc: '战略投资',
-    id: -50,
-    value: 'INFORMAL',
+    "desc": "Pre-A轮",
+    "id": 20,
+    "value": "PRE_A"
   },
   {
-    desc: '未融资',
-    id: 0,
-    value: 'NONE',
+    "desc": "A轮",
+    "id": 30,
+    "value": "A"
   },
   {
-    desc: '天使轮',
-    id: 10,
-    value: 'ANGEL',
+    "desc": "A+轮",
+    "id": 35,
+    "value": "A_PLUS"
   },
   {
-    desc: 'Pre-A轮',
-    id: 20,
-    value: 'PRE_A',
+    "desc": "B轮",
+    "id": 40,
+    "value": "B"
   },
   {
-    desc: 'A轮',
-    id: 30,
-    value: 'A',
+    "desc": "B+轮",
+    "id": 45,
+    "value": "B_PLUS"
   },
   {
-    desc: 'A+轮',
-    id: 35,
-    value: 'A_PLUS',
+    "desc": "C轮",
+    "id": 50,
+    "value": "C"
   },
   {
-    desc: 'B轮',
-    id: 40,
-    value: 'B',
+    "desc": "D轮",
+    "id": 60,
+    "value": "D"
   },
   {
-    desc: 'B+轮',
-    id: 45,
-    value: 'B_PLUS',
-  },
-  {
-    desc: 'C轮',
-    id: 50,
-    value: 'C',
-  },
-  {
-    desc: 'D轮',
-    id: 60,
-    value: 'D',
-  },
-  {
-    desc: 'E轮及以后',
-    id: 70,
-    value: 'E',
-  },
-  {
-    desc: '并购',
-    id: 100,
-    value: 'ACQUIRED',
-  },
-  {
-    desc: '上市',
-    id: 110,
-    value: 'IPO',
-  },
+    "desc": "E轮及以后",
+    "id": 70,
+    "value": "E"
+  }
 ];
 
 const COMPANY_SEARCH_PHASE = extractMeta(COMPANY_SEARCH_PHASE_META);
