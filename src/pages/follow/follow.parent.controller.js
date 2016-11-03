3ebf04db5f8e1ac596b4f46417d702f5a73c3224
zhouxiao <zhouxiao@36kr.com>
2016-11-03 14:32:17 +0800

    fix:我的关注修复

--- src/pages/follow/follow.parent.controller.js
+++ src/pages/follow/follow.parent.controller.js
@@ -280,6 +280,30 @@ export default class followParentController {
         });
       }
     });
+    this.handleLabel();
+  }
+
+  /*收起筛选器展示已选择的标签*/
+  handleLabel() {
+    var temp = {
+      industry: 'desc',
+      label: 'name',
+      phase: 'desc',
+      city: 'name',
+      isFundingLimit: 'name'
+    };
+    this.filterData = {};
+    Object.keys(this.data).forEach(key => {
+      angular.forEach(this.data[key], item => {
+        if(item.active && item.value !== 'unlimited' && item.id !== 'unlimited') {
+          if(this.filterData[key]) {
+            this.filterData[key] += ','+item[temp[key]];
+          }else {
+            this.filterData[key] = item[temp[key]];
+          }
+        }
+      });
+    });
   }
 
   /*筛选器选择行业*/
