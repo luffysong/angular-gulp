4dd0dec3901a6838cc55cfdf0882d428182ab487
songkaiyu <songkaiyu@36kr.com>
2016-11-08 15:52:32 +0800

    add mkfont

---
+++ build/mkfont/test.js
@@ -0,0 +1,113 @@
+/* eslint-disable */
+var page = require('webpage').create();
+var fs = require('fs');
+var system = require('system');
+var args = system.args;
+var files = null;
+fs.changeWorkingDirectory('./build/mkfont');
+console.log(fs.workingDirectory);
+if (args.length === 1) {
+  console.log('Try to pass some arguments when invoking this script!');
+  phantom.exit();
+} else {
+   files = JSON.parse(args[1]);
+}
+var expectedContent = fs.read('./index.html');
+var expectedLocation = 'https://icomoon.io/app/#/select/';
+page.setContent(expectedContent, expectedLocation);
+page.onLoadFinished = function(status) {
+  page.injectJs('./angular.min.js');
+  su = page.injectJs('./angular-ui-router.min.js');
+  page.injectJs('./idbstore.min.js');
+  page.injectJs('./FileSaver.min.js');
+  page.injectJs('./icon.js');
+};
+
+
+function selectAll() {
+    function makeEv(target) {
+       var ev = document.createEvent("MouseEvent");
+        ev.initMouseEvent(
+            "click",
+            true /* bubble */, true /* cancelable */,
+            window, null,
+            0, 0, 0, 0, /* coordinates */
+            false, false, false, false, /* modifier keys */
+            0 /*left*/, target
+        );
+        return ev;
+    }
+    var len = document.querySelectorAll('.set').length;
+    if(len === 1) return;
+    do {
+        var target = document.querySelector('#setH'+len + ' button.prs')
+        var ev = makeEv(target);
+        target.dispatchEvent(ev);
+    }while(--len > 1);
+    location.hash = '#/select/font';
+}
+function uploadFile(i) {
+    if(i >= files.length) {
+    setTimeout(function() {
+        page.evaluate(selectAll);
+    }, 10)
+        return;
+    }
+    page.uploadFile('#file input', files[i]);
+    setTimeout(function() {
+        uploadFile(++i);
+    }, 10)
+}
+
+
+function getFile(name) {
+    return 'file:///' + fs.workingDirectory + '/' + name;
+}
+
+page.onResourceRequested = function onResourceRequested(requestData, networkRequest) {
+    var match = requestData.url.match(/tmpl\/(.*)$/);
+    var match1 = requestData.url.match(/views\/(.*)$/);
+    if (/b9b9ae6a/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('icon.js'));
+    } else if(/app\/$/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('index.html'));
+    } else if(/FileSaver/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('FileSaver.min.js'));
+    } else if(/idbstore/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('idbstore.min.js'));
+    } else if(/angular-ui-router/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('angular-ui-router.min.js'));
+    } else if(/sha3d71416/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('angular.min.js'));
+    } else if(/main\.html/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('main.html'));
+    } else if(/colorpicker.html/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('colorpicker.html'));
+    } else if(/image.svg.tmpl/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('image.svg.tmpl'));
+    } else if(/getsessiontime/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('getsessiontime'));
+    } else if(/dict.json/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('dict.json'));
+    } else if(/presets.json/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('presets.json'));
+    } else if(/purchases/.test(requestData.url)) {
+        networkRequest.changeUrl(getFile('purchases'));
+    } else if(match) {
+        networkRequest.changeUrl(getFile(match[1]));
+    } else if(match1) {
+        networkRequest.changeUrl(getFile(match1[1]));
+    } else {
+        networkRequest.changeUrl(getFile('fake'));
+    }
+}
+page.onCallback = function(data) {
+    if (data === 'upload') {
+      console.log('upload start');
+      uploadFile(0);
+    } else {
+        console.log('@@kr-font@@' + JSON.stringify(data.files))
+        phantom.exit();
+    }
+
+};
