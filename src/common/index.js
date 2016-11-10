70f998706c3d4b4a754c6b1f0ad029e02ddb708f
songkaiyu <songkaiyu@36kr.com>
2016-11-10 14:16:02 +0800

    fix: 修复侧边栏 展开收起 ，content-moudle padding-left问题

--- src/common/index.js
+++ src/common/index.js
@@ -14,6 +14,10 @@ window.krData = {
   User,
 };
 window.krData.paddingContent = () => {
-  $('.content-wrapper').css('padding-left', `${$('#sidebar').width()}px`);
+  // 因为动画原因，不能使用width检测宽度；
+  const NARROW_WIDTH = 50;
+  const WIDEN_WIDTH = 140;
+  const width = $('#sidebar.narrow')[0] ? NARROW_WIDTH : WIDEN_WIDTH;
+  $('.content-wrapper').css('padding-left', `${width}px`);
 };
 require('./bootstrap');
