614f6481a73d870a8ff60515c34e813409cd055b
zhangliansheng <zls0926@gmail.com>
2016-11-14 11:41:33 +0800

    fix: 投资案例列表页简介限制只显示两行

--- src/pages/investment/introFormat.filter.js
+++ src/pages/investment/introFormat.filter.js
@@ -2,6 +2,9 @@
 function introFilter() {
   return function (input) {
     if(!input)return '';
+    if(input.length < 60){
+        return input;
+    }
     var format_input = input.substr(0, 60);
     return format_input + '...'
   }
