000259405263e3559e3e80616fab2b4c2e008b1e
zhangliansheng <zls0926@gmail.com>
2016-11-20 19:26:06 +0800

    fix: 修复投资人详情页投资案例不展示问题

--- src/pages/investorInfo/index.js
+++ src/pages/investorInfo/index.js
@@ -1,7 +1,7 @@
 import investorInfoController from './investorInfo.controller';
 import EditInvestorController from './investorInfoEdit.controller';
 import investorInfoService from './investorInfo.service';
-import dateFilter from './dateFilter.filter';
+import investDateFilter from './investDateFilter.filter';
 import briefFilter from './briefFilter.filter';
 import textOverflowDirective from '../project/textOverflow.directive.js';
 import krScrollClick from '../project/krScrollClick.directive.js';
@@ -10,7 +10,7 @@ angular.module('@@pages.investorInfo', [])
   .controller('investorInfoController', investorInfoController)
   .controller('EditInvestorController', EditInvestorController)
   .service('investorInfoService', investorInfoService)
-  .filter('dateFilter', dateFilter)
+  .filter('investDateFilter', investDateFilter)
   .filter('briefFilter', briefFilter)
   .directive('krScrollClick', krScrollClick)
   .directive('textOverflow', textOverflowDirective);
