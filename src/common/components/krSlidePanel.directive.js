426a06a89b5df97d0209de86208e6f1eff6c6163
zhouxiao <zhouxiao@36kr.com>
2016-11-21 17:07:06 +0800

    fix: 首页样式修复、系统消息组件修复

--- src/common/components/krSlidePanel.directive.js
+++ src/common/components/krSlidePanel.directive.js
@@ -1,30 +1,40 @@
+var krSlide = {};
+
 @Inject('$rootScope', '$scope')
 class SlidePanelController {
 
   constructor() {
     this.show = false;
     this.$scope.$on('$locationChangeStart', () => {
-      this.show = false;
-      $('html').removeClass('overflow-hidden');
+      this.hideModal();
     });
+    krSlide[this.$scope.key] = this;
   }
 
   watch() {
   }
 
-  trigger(e) {
+  showModal() {
+    for(var i in krSlide) {
+      krSlide[i].show = false;
+    }
+    this.show = true;
+    $('html').addClass('overflow-hidden');
+  }
+
+  hideModal() {
+    this.show = false;
+    $('html').removeClass('overflow-hidden');
+  }
+
+  trigger(e,type) {
     if (this.$scope.disabled) {
       return;
     }
     if (e) {
       e.preventDefault();
     }
-    this.show = !this.show;
-    if (this.show) {
-      $('html').addClass('overflow-hidden');
-    } else {
-      $('html').removeClass('overflow-hidden');
-    }
+    type === 'hover' || !this.show ? this.showModal() : this.hideModal();
   }
 
 }
@@ -37,10 +47,11 @@ export default {
   },
   scope: {
     disabled: '=disabled',
+    key: '@key'
   },
   template: `
     <div>
-      <div class="title" ng-transclude="trigger" ng-click="vm.trigger($event)">点我显示</div>
+      <div class="title" ng-transclude="trigger" ng-mouseenter="vm.trigger($event,'hover')">点我显示</div>
       <div class="kr-slide-panel" ng-class="vm.show?'show':''">
         <div class="bg" ng-click="vm.trigger($event)"></div>
         <div class="panel-body">
