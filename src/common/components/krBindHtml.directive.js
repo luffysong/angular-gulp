9e89ba486c0037ffdfb485d9491cae67a7281f41
zhouxiao <zhouxiao@36kr.com>
2016-11-24 21:44:57 +0800

    fix: 系统消息fix

--- src/common/components/krBindHtml.directive.js
+++ src/common/components/krBindHtml.directive.js
@@ -2,13 +2,24 @@ import { getService } from '../base/utls.js';
 export default {
   restrict: 'AE',
   link: function krBindHtmlPostLink($scope, ele, attr) {
-    const html = $scope.$eval(attr.krBindHtml);
-    if (angular.isString(html)) {
-      const $compile = getService('$compile');
-      ele.html(html);
-      $compile(ele.contents())($scope);
-    } else {
-      ele[0].innerHTML = getService('$sce').getTrustedHtml(html);
+
+    function handleHtml() {
+      var html = $scope.$eval(attr.krBindHtml);
+      if (angular.isString(html)) {
+        const $compile = getService('$compile');
+        ele.html(html);
+        $compile(ele.contents())($scope);
+      } else {
+        ele[0].innerHTML = getService('$sce').getTrustedHtml(html);
+      }
+    }
+
+    handleHtml();
+
+    if(attr.isWatch) {
+      $scope.$watch(attr.krBindHtml,o => {
+        handleHtml();
+      })
     }
   },
 };
