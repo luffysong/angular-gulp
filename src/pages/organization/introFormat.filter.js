cbd118e2136a6f5a630a28f7a05b75ed5426b35c
zhangliansheng <zls0926@gmail.com>
2016-11-29 11:55:25 +0800

    fix: 修复机构列表简介显示不正确问题

--- src/pages/organization/introFormat.filter.js
+++ src/pages/organization/introFormat.filter.js
@@ -2,6 +2,9 @@
 function introFilter() {
   return function (input) {
     if(!input)return '';
+    if(input.length < 55){
+      return input;
+    }
     var format_input = input.substr(0, 55);
     return format_input + '...'
   }
