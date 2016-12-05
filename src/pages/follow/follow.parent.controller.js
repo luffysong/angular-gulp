f1e28be7e7f72447d519b7a9e1b3c42823957662
zhouxiao <zhouxiao@36kr.com>
2016-12-05 15:38:39 +0800

    fix: eslint fix

--- src/pages/follow/follow.parent.controller.js
+++ src/pages/follow/follow.parent.controller.js
@@ -48,12 +48,12 @@ export default class followParentController {
       isFundingLimit: [
         {
           id: 'unlimited',
-          name: '不限'
-        },{
+          name: '不限',
+        }, {
           id: 1,
-          name: '融资中'
-        }
-      ]
+          name: '融资中',
+        },
+      ],
     };
 
     this.$scope.$on('get-change', (e, d) => {
@@ -77,7 +77,7 @@ export default class followParentController {
       }
     });
 
-    this.$scope.$on('open-sideBar',(e,d) => {
+    this.$scope.$on('open-sideBar', (e, d) => {
       d.labels = this.followLabel;
       d.switchType = this.switchType.bind(this);
       this.columnOptions = d;
@@ -250,21 +250,21 @@ export default class followParentController {
       }
     });
     angular.forEach(this.itemList,(item) => {
-      /*筛选条件特殊处理*/
-      if(item.name === 'isFundingLimit'){
-        angular.forEach(d.funding,(c,index) => {
-          angular.forEach(this.data['isFundingLimit'],k => {
-            if(k.name === c.name || k.id+'' === c.id+'') {
+      /* 筛选条件特殊处理 */
+      if (item.name === 'isFundingLimit') {
+        angular.forEach(d.funding, (c) => {
+          angular.forEach(this.data.isFundingLimit, k => {
+            if (k.name === c.name || `${k.id}` === `${c.id}`) {
               k.cnt = c.cnt;
             }
-          })
+          });
         });
         return;
       }
-      if(!d[item.name] || !d[item.name].length)return;
+      if (!d[item.name] || !d[item.name].length) return;
       angular.forEach(d[item.name],(obj) => {
-        angular.forEach(this.data[item.name],(c) => {
-          if((c[item.key] && c[item.key] === obj[item.key]) || (obj.name === '不限' && (c.name === '不限' || c.desc === '不限'))){
+        angular.forEach(this.data[item.name], (c) => {
+          if ((c[item.key] && c[item.key] === obj[item.key]) || (obj.name === '不限' && (c.name === '不限' || c.desc === '不限'))) {
             c.cnt = obj.cnt;
           }
         });
@@ -272,30 +272,30 @@ export default class followParentController {
     })
   }
 
-  /*根据路由参数处理激活*/
-  handleActive () {
+  /* 根据路由参数处理激活 */
+  handleActive() {
     this.dataInit();
-    angular.forEach(this.params,(val,key) => {
-      if(key === 'labelId' && val) {
-        angular.forEach(this.followLabel,item => {
-          if(item.id+'' === val) {
+    angular.forEach(this.params, (val, key) => {
+      if (key === 'labelId' && val) {
+        angular.forEach(this.followLabel, item => {
+          if (`${item.id}` === val) {
             item.active = true;
-          }else {
+          } else {
             item.active = false;
           }
         });
       }
-      if(val && val.split(',').length > 1){
-        angular.forEach(val.split(','),(a) => {
-          angular.forEach(this.data[key],(item,index) => {
-            if(item.value+'' === a+'' || item.id+'' === a+'') {
+      if (val && val.split(',').length > 1) {
+        angular.forEach(val.split(','), (a) => {
+          angular.forEach(this.data[key], (item) => {
+            if (`${item.value}` === `${a}` || `${item.id}` === `${a}`) {
               item.active = true;
             }
           });
         });
-      }else {
-        angular.forEach(this.data[key],(item,index) => {
-          if(item.value+'' === val+'' || item.id+'' === val+'') {
+      } else {
+        angular.forEach(this.data[key], (item) => {
+          if (`${item.value}` === `${val}` || `${item.id}` === `${val}`) {
             item.active = true;
           }
         });
@@ -304,22 +304,22 @@ export default class followParentController {
     this.handleLabel();
   }
 
-  /*收起筛选器展示已选择的标签*/
+  /* 收起筛选器展示已选择的标签 */
   handleLabel() {
-    var temp = {
+    const temp = {
       industry: 'desc',
       label: 'name',
       phase: 'desc',
       city: 'name',
-      isFundingLimit: 'name'
+      isFundingLimit: 'name',
     };
     this.filterData = {};
     Object.keys(this.data).forEach(key => {
       angular.forEach(this.data[key], item => {
-        if(item.active && item.value !== 'unlimited' && item.id !== 'unlimited') {
-          if(this.filterData[key]) {
+        if (item.active && item.value !== 'unlimited' && item.id !== 'unlimited') {
+          if (this.filterData[key]) {
             this.filterData[key] += ','+item[temp[key]];
-          }else {
+          } else {
             this.filterData[key] = item[temp[key]];
           }
         }
@@ -327,39 +327,40 @@ export default class followParentController {
     });
   }
 
-  /*筛选器选择行业*/
-  selectIndustry (index,type) {
-    /*筛选器选择不限*/
-    if(this.data[type][index].name === '不限' || this.data[type][index].desc === '不限') {
+  /* 筛选器选择行业 */
+  selectIndustry(index, type) {
+    /* 筛选器选择不限 */
+    if (this.data[type][index].name === '不限' || this.data[type][index].desc === '不限') {
       this.params[type] = 'unlimited';
       this.go();
       return;
     }
-    var attr = type === 'city' || type === 'isFundingLimit' || type === 'label' ? 'id' : 'value';
-    if(this.params[type]){
-      if(this.params[type].split(',').indexOf(String(this.data[type][index][attr])) < 0) {
-        var arr = this.params[type].split(',');
-        if(arr.indexOf('unlimited') >= 0){arr.splice(arr.indexOf('unlimited'),1)};
+    const attr = type === 'city' || type === 'isFundingLimit' || type === 'label' ? 'id' : 'value';
+    if (this.params[type]) {
+      if (this.params[type].split(',').indexOf(String(this.data[type][index][attr])) < 0) {
+        const arr = this.params[type].split(',');
+        if (arr.indexOf('unlimited') >= 0) {
+          arr.splice(arr.indexOf('unlimited'), 1);
+        }
         arr.push(this.data[type][index][attr]);
         this.params[type] = arr.join(',');
       }
-    }else {
+    } else {
       this.params[type] = this.data[type][index][attr];
     }
     this.go();
   }
 
-  /*取消选择行业*/
-  clearIndustry (id,type) {
-    var arr = this.params[type].split(',');
-    arr.splice(arr.indexOf(id+''),1);
+  /* 取消选择行业 */
+  clearIndustry(id, type) {
+    const arr = this.params[type].split(',');
+    arr.splice(arr.indexOf(`${id}`), 1);
     this.params[type] = arr.join(',');
-    /*this.params[type] = null;*/
     this.go();
   }
 
-  /*state跳转*/
-  go () {
+  /* state跳转 */
+  go() {
     this.$state.go('follow.result', this.params);
   }
 
@@ -367,16 +368,16 @@ export default class followParentController {
     krData.utls.login();
   }
 
-  /*增加默认数据*/
-  addItem(obj,type) {
-    if(!obj || !obj.concat)return;
-    var temp = obj.concat();
-    var c = {
+  /* 增加默认数据 */
+  addItem(obj, type) {
+    if (!obj || !obj.concat) return;
+    const temp = obj.concat();
+    const c = {
       active: false,
       id: 0,
-      value: 'unlimited'
+      value: 'unlimited',
     };
-    if( type === 'city'){
+    if (type === 'city') {
       c.name = '不限';
     } else {
       c.desc = '不限';
@@ -385,19 +386,19 @@ export default class followParentController {
     return temp;
   }
 
-  /*数据active全部初始化*/
-  dataInit () {
+  /* 数据active全部初始化 */
+  dataInit() {
     Object.keys(this.data).forEach((item) => {
-      angular.forEach(this.data[item],(obj) => {
+      angular.forEach(this.data[item], (obj) => {
         obj.active = false;
       });
-      if(!this.params[item] && this.data[item].length) {
+      if (!this.params[item] && this.data[item].length) {
         this.data[item][0].active = true;
       }
     });
   };
 
-  /*获取静态城市数据*/
+  /* 获取静态城市数据 */
   getCity() {
     this.projectService.getArea(0)
       .then(data => {
@@ -407,13 +408,13 @@ export default class followParentController {
 
   }
 
-  /*获取静态行业数据*/
+  /* 获取静态行业数据 */
   getIndustry() {
     this.data.industry = this.addItem(this.$scope.root.INDUSTRY_META);
   }
 
 
-  /*获取轮次静态数据*/
+  /* 获取轮次静态数据 */
   getPhase() {
     this.data.phase = this.addItem(this.$scope.root.FUNDS_PHASE_ENUM_META);
   }
