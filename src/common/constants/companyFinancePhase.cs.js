29aac02980da0b2469bd97518ec0000bb87d446b
zhouxiao <zhouxiao@36kr.com>
2016-12-05 10:51:46 +0800

    feat: 字典更新

--- src/common/constants/companyFinancePhase.cs.js
+++ src/common/constants/companyFinancePhase.cs.js
@@ -1,75 +1,80 @@
 import { extractMeta } from '../base/utls';
 const COMPANY_FINANCE_PHASE_META = [
   {
-    desc: '未知轮次',
-    id: -100,
-    value: 'UNKNOWN',
+    'desc': '未知轮次',
+    'id': -100,
+    'value': 'UNKNOWN'
   },
   {
-    desc: '战略投资',
-    id: -50,
-    value: 'INFORMAL',
+    'desc': '战略投资',
+    'id': -50,
+    'value': 'INFORMAL'
   },
   {
-    desc: '未融资',
-    id: 0,
-    value: 'NONE',
+    'desc': '新三板',
+    'id': -10,
+    'value': 'NEEQ'
   },
   {
-    desc: '天使轮',
-    id: 10,
-    value: 'ANGEL',
+    'desc': '未融资',
+    'id': 0,
+    'value': 'NONE'
   },
   {
-    desc: 'Pre-A轮',
-    id: 20,
-    value: 'PRE_A',
+    'desc': '天使轮',
+    'id': 10,
+    'value': 'ANGEL'
   },
   {
-    desc: 'A轮',
-    id: 30,
-    value: 'A',
+    'desc': 'Pre-A轮',
+    'id': 20,
+    'value': 'PRE_A'
   },
   {
-    desc: 'A+轮',
-    id: 35,
-    value: 'A_PLUS',
+    'desc': 'A轮',
+    'id': 30,
+    'value': 'A'
   },
   {
-    desc: 'B轮',
-    id: 40,
-    value: 'B',
+    'desc': 'A+轮',
+    'id': 35,
+    'value': 'A_PLUS'
   },
   {
-    desc: 'B+轮',
-    id: 45,
-    value: 'B_PLUS',
+    'desc': 'B轮',
+    'id': 40,
+    'value': 'B'
   },
   {
-    desc: 'C轮',
-    id: 50,
-    value: 'C',
+    'desc': 'B+轮',
+    'id': 45,
+    'value': 'B_PLUS'
   },
   {
-    desc: 'D轮',
-    id: 60,
-    value: 'D',
+    'desc': 'C轮',
+    'id': 50,
+    'value': 'C'
   },
   {
-    desc: 'E轮及以后',
-    id: 70,
-    value: 'E',
+    'desc': 'D轮',
+    'id': 60,
+    'value': 'D'
   },
   {
-    desc: '并购',
-    id: 100,
-    value: 'ACQUIRED',
+    'desc': 'E轮及以后',
+    'id': 70,
+    'value': 'E'
   },
   {
-    desc: '上市',
-    id: 110,
-    value: 'IPO',
+    'desc': '并购',
+    'id': 100,
+    'value': 'ACQUIRED'
   },
+  {
+    'desc': '上市',
+    'id': 110,
+    'value': 'IPO'
+  }
 ];
 
 const COMPANY_FINANCE_PHASE = extractMeta(COMPANY_FINANCE_PHASE_META);
