8e6ad74f0821ce304f2563b4862513eead71db3b
zhouxiao <zhouxiao@36kr.com>
2017-01-04 16:15:30 +0800

    fix: esLint fix

--- src/pages/investorValidate/investorValidate.controller.js
+++ src/pages/investorValidate/investorValidate.controller.js
@@ -4,8 +4,6 @@ import ProjectService from '../project/project.service';
 @Inject('$sce', '$scope', '$q', '$stateParams', '$state', '$validation')
 export default class investorValidateController {
 
-
-
   constructor() {
     this.init();
   }
@@ -13,9 +11,8 @@ export default class investorValidateController {
   projectService = new ProjectService();
 
   init() {
-
     this.baseInfo = {
-      singleInvestUnit: 'CNY'
+      singleInvestUnit: 'CNY',
     };
 
     this.auditStatus = '';
@@ -29,29 +26,21 @@ export default class investorValidateController {
     this.step = 1;
 
     this.currentIndex = 0;
-    //this.getSuggestInvestor();
-
-    /*this.$scope.$watch('vm.suggestInvestor[0].industryEnumList',obj => {
-      console.log(obj);
-    },true);*/
 
     this.autocompleteOptions = {
       suggest: this.suggest.bind(this),
       on_select: this.onSelect.bind(this),
       auto_select_first: true,
       full_match: (item, word) => {
-        if(item && item.value) {
+        if (item && item.value) {
           return item.value.toLowerCase() === word.toLowerCase();
         }
-      }
-
-
+      },
     };
 
     this.getUser();
     this.setBiggerValidator();
     this.watchInvestAmountMin();
-
   }
 
   suggest(kw) {
@@ -121,10 +110,10 @@ export default class investorValidateController {
   }
 
   selectItem(index) {
-    this.suggestInvestor.forEach((item,i) => {
-      if(index+'' === i+'') {
+    this.suggestInvestor.forEach((item, i) => {
+      if (`${index}` === `${i}`) {
         item.active = !item.active;
-      }else {
+      } else {
         item.active = false;
       }
     });
@@ -134,7 +123,7 @@ export default class investorValidateController {
   getField() {
     this.initField();
     this.suggestInvestor.forEach((item,index) => {
-      if(item.active) {
+      if (item.active) {
         this.baseInfo.selectedIndustry = angular.copy(this.suggestInvestor[index].industryEnumList);
         this.baseInfo.selectedPhase = angular.copy(this.suggestInvestor[index].phaseEnumList);
         this.baseInfo.relatedId = angular.copy(this.suggestInvestor[index].id);
@@ -143,7 +132,8 @@ export default class investorValidateController {
         this.baseInfo.relatedPosition = angular.copy(this.suggestInvestor[index].position);
         this.baseInfo.singleInvestMin = angular.copy(this.suggestInvestor[index].singleInvestMin);
         this.baseInfo.singleInvestMax = angular.copy(this.suggestInvestor[index].singleInvestMax);
-        this.baseInfo.singleInvestUnit = this.suggestInvestor[index].singleInvestUnit ? angular.copy(this.suggestInvestor[index].singleInvestUnit) : 'CNY';
+        this.baseInfo.singleInvestUnit = this.suggestInvestor[index].singleInvestUnit ?
+          angular.copy(this.suggestInvestor[index].singleInvestUnit) : 'CNY';
       }
     });
   }
@@ -162,19 +152,13 @@ export default class investorValidateController {
 
   getUser() {
     krData.User.getUserInfo().then(data => {
-      //delete data.phone;
       this.userData = data;
-
       this.getState();
-
     }).catch(err => {
-      if(err.code === 403) {
+      if (err.code === 403) {
         this.$scope.root.user.ensureLogin();
       }
     });
-    /*this.user.then(data => {
-      console.log(data);
-    });*/
   }
 
   reSubmit() {
@@ -184,55 +168,52 @@ export default class investorValidateController {
     this.projectService.getState().then(data => {
       switch (data.state) {
         case 'PENDING':
-              this.step = 3;
-              this.auditStatus = 'auditing';
-              break;
+          this.step = 3;
+          this.auditStatus = 'auditing';
+          break;
         case 'PASS':
-              this.step = 3;
-              this.auditStatus = 'audited';
-              break;
+          this.step = 3;
+          this.auditStatus = 'audited';
+          break;
         case 'REJECT':
-              this.step = 3;
-              this.auditStatus = 'auditFail';
-              this.failReason = data.reason;
-              break;
+          this.step = 3;
+          this.auditStatus = 'auditFail';
+          this.failReason = data.reason;
+          break;
 
       }
-    }).catch(err => {
-    });
+    }).catch();
   }
 
   getCode() {
     // 获取验证码
-      this.wait = 60;
-      var interval = setInterval(() => {
-        this.$scope.$apply(() => {
-          this.wait--;
-          if (this.wait === 0) {
-            clearInterval(interval);
-            this.wait = 0;
-          }
-        });
-      }, 1000);
-      this.projectService.getMsgCode({
-        phone: this.baseInfo.phone
-      }).then(data => {
-      }).catch(err => {
+    this.wait = 60;
+    const interval = window.setInterval(() => {
+      this.$scope.$apply(() => {
+        this.wait--;
+        if (this.wait === 0) {
+          clearInterval(interval);
+          this.wait = 0;
+        }
       });
+    }, 1000);
+    this.projectService.getMsgCode({
+      phone: this.baseInfo.phone,
+    });
   }
 
   validatePhone() {
-    /*用户已绑定手机号*/
-    if(this.userData.phone){
+    /* 用户已绑定手机号 */
+    if (this.userData.phone){
       this.getSuggestInvestor();
       this.step = 2;
       return;
     }
-    /*验证手机号*/
+    /* 验证手机号 */
     this.projectService.validateMsgCode({
       code: this.baseInfo.code,
-      phone: this.baseInfo.phone
-    }).then(data => {
+      phone: this.baseInfo.phone,
+    }).then(() => {
       this.codeError = false;
       this.getSuggestInvestor();
     }).catch(err => {
@@ -248,22 +229,20 @@ export default class investorValidateController {
     }).then(data => {
       this.suggestInvestor = data;
       this.step = 2;
-    }).catch(err => {
     });
   }
 
 
-  /*验证手机号是否合法*/
+  /* 验证手机号是否合法 */
   submitInfo() {
     this.$validation.validate(this.baseInfoForm).catch(() => {
       return this.$q.reject();
     }).then(() => {
       this.validatePhone();
     });
-    /*if(!this.checkForm('baseInfoForm'))return;*/
   }
 
-  /*提交认证*/
+  /* 提交认证 */
   submitValidate() {
     this.$validation.validate(this.financeInfo.form).catch(() => {
       return this.$q.reject();
@@ -273,49 +252,38 @@ export default class investorValidateController {
   }
 
   submitRequest() {
-    var params = Object.assign({
+    const params = Object.assign({
       phaseEnumList: this.baseInfo.selectedPhase.join(','),
       industryEnumList: this.baseInfo.selectedIndustry.join(',')
-    },this.baseInfo);
+    }, this.baseInfo);
     delete params.selectedPhase;
     delete params.selectedIndustry;
-    /*个人投资人过滤投资机构和职位*/
-    if(params.investorRoleEnum !== 'ORG_INVESTOR') {
+    /* 个人投资人过滤投资机构和职位 */
+    if (params.investorRoleEnum !== 'ORG_INVESTOR') {
       delete params.orgName;
       delete params.orgId;
       delete params.position;
     }
-    console.warn(params);
-    this.projectService.submitValidate(params).then(data => {
+    this.projectService.submitValidate(params).then(() => {
       this.step = 3;
       this.auditStatus = 'auditing';
     }).catch(err => {
-      if(err && err.msg) {
+      if (err && err.msg) {
         krData.Alert.alert(err.msg);
       }
     });
   }
 
   uploadBp($files) {
-    const name = this.$scope.vm.baseInfo.name || this.name;
-    let validObj = null;
     if ($files.length) {
-     /* validObj = krData.utls.validateBP($files[0]);
-      if (!validObj.valid) {
-        krData.Alert.alert(validObj.msg);
-        return;
-      }*/
       krData.utls.uploadImage($files[0],{
-        maxSize: '5'
-      })
-        .then(data => {
-          this.baseInfo.businessCard = data.src;
-          /*this.finance.bp = data.src;
-          this.bpName = `[${name}]商业计划书.pdf`;*/
-        }, err => {
-          this.bpUploading = false;
-          krData.Alert.alert(`上传名片失败:${err.msg}`);
-        }).then(null, null, (progress) => {
+        maxSize: '5',
+      }).then(data => {
+        this.baseInfo.businessCard = data.src;
+      }, err => {
+        this.bpUploading = false;
+        krData.Alert.alert(`上传名片失败:${err.msg}`);
+      }).then(null, null, (progress) => {
         if (progress.type === 'load') {
           this.bpProgress = '100%';
           this.bpUploading = false;
@@ -326,6 +294,4 @@ export default class investorValidateController {
       });
     }
   }
-
-
 }
