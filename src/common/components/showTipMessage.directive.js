cf6764f4a1218fc9e55b67598b029e5dd33462a9
zhangliansheng <zls0926@gmail.com>
2017-03-07 16:34:55 +0800

    fix: 申请投资人添加引导文案

--- src/common/components/showTipMessage.directive.js
+++ src/common/components/showTipMessage.directive.js
@@ -4,27 +4,38 @@ function showTipMessage($compile) {
     restrict: 'AE',
     link(scope, elem, attrs) {
       if(elem[0].type === 'textarea'){
-        elem[0].onfocus = function () {
-          elem.parent().parent().children()[0].style.bottom="143px";
-          elem.parent().parent().children()[0].style.paddingBottom="40px";
-          elem.parent().parent().children()[0].style.display="block";
-        };
-        elem[0].onblur = function () {
-          elem.parent().parent().children()[0].style.display="none";
-        };
-        elem.parent().parent().children().children()[0].onclick = function (){
-          elem.parent().parent().children()[0].style.display="none";
-        };
+        if(elem.parent().children('.tip-message').children()[0]){
+          elem[0].onfocus = function () {
+            elem.parent().parent().children()[0].style.bottom="143px";
+            elem.parent().parent().children()[0].style.paddingBottom="40px";
+            elem.parent().parent().children()[0].style.display="block";
+          };
+          elem[0].onblur = function () {
+            elem.parent().parent().children()[0].style.display="none";
+          };
+          elem.parent().parent().children().children()[0].onclick = function (){
+            elem.parent().parent().children()[0].style.display="none";
+          };
+        }
       } else {
-        elem[0].onfocus = function () {
-          elem.parent().children()[0].style.display="block";
-        };
-        elem[0].onblur = function () {
-          elem.parent().children()[0].style.display="none";
-        };
-        elem.parent().children().children()[0].onclick = function (){
-          elem.parent().children()[0].style.display="none";
-        };
+        console.log(elem.parent().children('.error-tip-message'));
+        if(elem.parent().children('.tip-message').children()[0]){
+          elem[0].onfocus = function () {
+            elem.parent().children()[0].style.display="block";
+          };
+          elem[0].onblur = function () {
+            elem.parent().children()[0].style.display="none";
+          };
+          elem.parent().children().children()[0].onclick = function (){
+            elem.parent().children()[0].style.display="none";
+          };
+        }
+        if(elem.parent().children('.error-tip-message').children()[0]){
+          console.log(1111111111);
+          elem.parent().children('.error-tip-message').children()[0].onclick = function (){
+            elem.parent().children('.error-tip-message')[0].style.display="none";
+          };
+        }
       }
     }
   };
