2e667b1943a2ac439ae898add8cbc14db00d88d2
songyongqiang <songyongqiang@36kr.com>
2017-03-09 15:38:44 +0800

    fix:添加查看更多

--- src/pages/thirdparty/index.js
+++ src/pages/thirdparty/index.js
@@ -1,7 +1,9 @@
 import ThirdpartyIndexController from './ThirdpartyIndexController.controller';
 import ThirdpartyIndexService from './ThirdpartyIndexService.service';
+import textOverflowDirective from '../project/textOverflow.directive';
 import CreateProjectService from '../createProject/createProject.service.js';
 angular.module('@@pages.thirdparty', [])
   .controller('ThirdpartyIndexController', ThirdpartyIndexController)
+  .directive('textOverflow', textOverflowDirective)
   .service('createProjectService', CreateProjectService)
   .service('thirdpartyIndexService', ThirdpartyIndexService);
