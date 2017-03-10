6c979670e7d321c5c3c7b75f014c6bc84e16b609
zhangliansheng <zls0926@gmail.com>
2017-03-10 17:00:00 +0800

    fix: 修改UI走查

--- src/pages/createProject/createProject.controller.js
+++ src/pages/createProject/createProject.controller.js
@@ -519,7 +519,7 @@ export default class CreateProjectController {
         label: `<div>
           <div class="suggest-logo"><img src='${com.logo}' /></div>
           <div class="suggest-project-text">
-            <p><span>${com.name}</span><span>${that.getIndustry(com.industry)}</span></p>
+            <p><span class="suggest-name">${com.name}</span><span class="suggest-industry">${that.getIndustry(com.industry)}</span></p>
             <p class="brief">${com.brief}</p>
           </div>
           </div> `,
