54abf8da882bd65b3f0656320d8f63ac3b88bf04
zhangliansheng <zls0926@gmail.com>
2017-04-25 17:04:19 +0800

    fix: 工商信息添加查看验证

--- src/pages/commercial/commercialIndex.controller.js
+++ src/pages/commercial/commercialIndex.controller.js
@@ -1,24 +1,29 @@
 import krData from 'krData';
 import CommercialService from './Commercial.service';
 @Inject('$stateParams', 'resolveData', '$validation', '$scope', '$sce',
- '$state', '$q', '$filter', 'ngDialog', 'user')
+    '$state', '$q', '$filter', 'ngDialog', 'user')
 export default class commercialIndexController {
-  constructor() {
-    this.projectId = $stateParams.id;
-    this.init();
-  }
+    constructor() {
+        this.projectId = $stateParams.id;
+        this.init();
+        this.limit = false;
+    }
 
-  commercialService = new CommercialService();
+    commercialService = new CommercialService();
 
-  init() {
+    init() {
+        const vm = this;
+        this.commercialService.getInfo(this.$stateParams.id).then(data => {
+            this.data = data;
+        }, function(error) {
+            if (error.code === 403) {
+                vm.limit = true;
+            }
+        });
 
-    this.commercialService.getInfo(this.$stateParams.id).then(data => {
-        this.data = data;
-      });
+        this.commercialService.getBaseInfo(this.$stateParams.id).then(data => {
+            this.baseInfo = data;
+        })
 
-    this.commercialService.getBaseInfo(this.$stateParams.id).then(data => {
-        this.baseInfo = data;
-    })
-
-  }
+    }
 }
